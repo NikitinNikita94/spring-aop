80528f80adfb290f2031302952163229620d9d17
nikita <7jay94@mail.ru>
2024-06-28 17:59:06 +0200

    init: init project spring-aop.

---
+++ src/test/java/t1/springaop/SpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package t1.springaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
